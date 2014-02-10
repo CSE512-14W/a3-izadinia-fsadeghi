94b1c225bae824d076507ac56f18f5d1996ae181
izadinia <izadinia@gmail.com>
2014-02-09 21:29:53 -0800

    The first submission.

---
+++ graph.js
@@ -0,0 +1,166 @@
+var re = "";
+var m0;
+
+function init()
+{
+  var diameter = 900,
+      radius = diameter / 2,
+      innerRadius = radius - 120;
+
+  var cluster = d3.layout.cluster()
+      .size([360, innerRadius])
+      .sort(null)
+      .value(function(d) { return d.size; });
+
+  var bundle = d3.layout.bundle();
+
+  var line = d3.svg.line.radial()
+      .interpolate("bundle")
+      .tension(.90) //.85
+      .radius(function(d) { return d.y; })
+      .angle(function(d) { return d.x / 180 * Math.PI; });
+  var svg = d3.select("svg")
+      .attr("width", diameter)
+      .attr("height", diameter)
+    .append("g")
+      .attr("transform", "translate(" + radius + "," + radius + ")");
+
+  var link = svg.append("g").selectAll(".link"),
+      node = svg.append("g").selectAll(".node");
+
+  var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-20, 0])
+  .html(function(d) {
+    return "<p><strong></strong> <span style='color:red'>" + d.key + "</span></p><p><img src='thumbnails/" + d.key + ".jpg' height='100' border='5'></p>";
+  })
+
+  svg.call(tip);
+
+ d3.json("mysunjson_pruned4high_thresh_clusty_.json", function(error, classes) {
+  // d3.json("readme-flare-imports.json", function(error, classes) {
+    var nodes = cluster.nodes(packageHierarchy(classes)),
+        links = packageImports(nodes);
+
+    link = link
+        .data(bundle(links))
+      .enter().append("path")
+        .each(function(d) { d.source = d[0], d.target = d[d.length - 1]; })
+        .attr("class", "link")
+        .attr("d", line);
+
+    node = node
+        .data(nodes.filter(function(n) { return !n.children; }))
+      .enter().append("text")
+        .attr("class", "node")
+        .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
+        .attr("dy", ".31em")
+        .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")" + (d.x < 180 ? "" : "rotate(180)"); })
+        .style("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+        .text(function(d) { return d.key; })
+        //.classed("node--target", function(d) { return d.key.match(re) ? true: false })
+        .on("mouseover", mouseovered)
+        .on("mouseout", mouseouted)
+        .on("click", tip.show);
+        // .on("mouseout", tip.hide);
+    
+    d3.select("input[type=range]").on("change", function() {
+      var svg = d3.select("svg")
+      var line = d3.svg.line.radial()
+      .interpolate("bundle")
+      .tension(this.value / 100) //.85
+      .radius(function(d) { return d.y; })
+      .angle(function(d) { return d.x / 180 * Math.PI; });
+
+      var link = svg.selectAll("path");
+      link.attr("d", line);
+    });
+});
+
+  
+
+
+  function mouseovered(d) {
+    node
+        .each(function(n) { n.target = n.source = false; });
+
+    link
+        .classed("link--target", function(l) { if (l.target === d) return l.source.source = true; })
+        .classed("link--source", function(l) { if (l.source === d) return l.target.target = true; })
+      .filter(function(l) { return l.target === d || l.source === d; })
+        .each(function() { this.parentNode.appendChild(this); });
+
+    node
+        .classed("node--target", function(n) { return n.target; })
+        .classed("node--source", function(n) { return n.source; });
+  }
+
+  function mouseouted(d) {
+    link
+        .classed("link--target", false)
+        .classed("link--source", false);
+
+    node
+        .classed("node--target", false)
+        .classed("node--source", false);
+    tip.hide();
+  }
+
+  d3.select(self.frameElement).style("height", diameter + "px");
+
+  // Lazily construct the package hierarchy from class names.
+  function packageHierarchy(classes) {
+    var map = {};
+
+    function find(name, data) {
+      var node = map[name], i;
+      if (!node) {
+        node = map[name] = data || {name: name, children: []};
+        if (name.length) {
+          node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+          node.parent.children.push(node);
+          node.key = name.substring(i + 1);
+        }
+      }
+      return node;
+    }
+
+    classes.forEach(function(d) {
+      find(d.name, d);
+    });
+
+    return map[""];
+  }
+
+  // Return a list of imports for the given array of nodes.
+  function packageImports(nodes) {
+    var map = {},
+        imports = [];
+
+    // Compute a map from name to node.
+    nodes.forEach(function(d) {
+      map[d.name] = d;
+    });
+
+    // For each import, construct a link from the source to target node.
+    nodes.forEach(function(d) {
+      if (d.imports) d.imports.forEach(function(i) {
+        imports.push({source: map[d.name], target: map[i]});
+      });
+    });
+
+    return imports;
+  }
+}
+
+function update(query) {
+  var svg = d3.select("svg")
+  var node = svg.selectAll(".node");  
+  
+  if (query != re) {  
+    re = new RegExp(query, "i");
+
+    node
+        .classed("node--search", function(n) { return n.key.match(re) ? true: false; });
+  }
+}
